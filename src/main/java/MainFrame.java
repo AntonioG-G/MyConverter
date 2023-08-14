0f4d531f5b4f3471beb8f47573012b3a2b869d14
Antonio Guerrero <antonioguerrero2109@gmail.com>
2023-08-14 06:01:42 -0600

    Estilo terminado, lógica por iniciar

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -1,15 +1,15 @@
 package main.java;
 
+import javafx.animation.ScaleTransition;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -17,6 +17,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.util.converter.IntegerStringConverter;
 
 public class MainFrame extends Application {
 
@@ -32,45 +34,25 @@ public class MainFrame extends Application {
 
         // Crear los botones
         Button button1 = new Button("Divisas");
+        //Función de los botones
         button1.setOnAction(e -> cConverter(primaryStage));
+        //Animación de hover de los botones
+        configureButtonAnimation(button1);
+
         Button button2 = new Button("Temperatura");
         button2.setOnAction(e -> dConverter());
+        configureButtonAnimation(button2);
+
         Button button3 = new Button("Distancia");
         button3.setOnAction(e -> tConverter());
+        configureButtonAnimation(button3);
         
-        // Establecer el estilo normal del botón
-        button1.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        button2.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        button3.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
+        //Adición de las clases de CSS de los botones para compartir estilos
+        button1.getStyleClass().add("button-style");
+        button2.getStyleClass().add("button-style");
+        button3.getStyleClass().add("button-style");
 
-        // Agregar un efecto hover utilizando eventos de mouse
-        button1.setOnMouseEntered(event -> {
-            button1.setStyle("-fx-background-color: #050505; -fx-text-fill: white;");
-        });
-        button2.setOnMouseEntered(event -> {
-            button2.setStyle("-fx-background-color: #050505; -fx-text-fill: white;");
-        });
-        button3.setOnMouseEntered(event -> {
-            button3.setStyle("-fx-background-color: #050505; -fx-text-fill: white;");
-        });
-
-        button1.setOnMouseExited(event -> {
-            button1.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        });
-        button2.setOnMouseExited(event -> {
-            button2.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        });
-        button3.setOnMouseExited(event -> {
-            button3.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        });
-        
-        // Cambia el tamaño de fuente del botón
-        button1.setFont(new Font(20));
-        button2.setFont(new Font(20));
-        button3.setFont(new Font(20));
-
-
-        // Crear la sección de los botones y el pie de página
+        // Crear la sección de los botones
         HBox buttonsSection = new HBox(80, button1, button2, button3);
         buttonsSection.setAlignment(Pos.CENTER);
 
@@ -81,28 +63,23 @@ public class MainFrame extends Application {
         
         //Crear la sección del footer
         Text footerText = new Text("Por Antonio Guerrero para AluraLatam y Oracle, 2023");
-        footerText.setFont(Font.font(18));
+        footerText.getStyleClass().add("footer-text");
+
+        //Crear la sección del footer
         HBox footer = new HBox(footerText);
+        footer.getStyleClass().add("footer");
         footer.setAlignment(Pos.BOTTOM_CENTER);
-        footer.setPadding(new Insets(20, 0, 20, 0));
-        
-        //color del footer
-        BackgroundFill backgroundF = new BackgroundFill(Color.rgb(199,70,52), null, null);
-        Background background2 = new Background(backgroundF);
-        footer.setBackground(background2);
-        
+        /* footer.setPadding(new Insets(20, 0, 20, 0)); */
         
         // Crear el contenedor principal
         VBox root = new VBox(80, contentSection, footer);
         root.setAlignment(Pos.BOTTOM_CENTER);
+        root.getStyleClass().add("root");
         
-        //Color del Root
-        BackgroundFill backgroundR = new BackgroundFill(Color.rgb(22,21,19), null, null);
-        Background background1 = new Background(backgroundR);
-        root.setBackground(background1);
-
         // Crear la escena y configurarla en el escenario
         Scene scene = new Scene(root, 960, 540);
+        scene.getStylesheets().add("file:///A:/programacion/java/MyConversorbyAntonioGG/MyConverterAlura/src/main/resources/style.css");
+
 
         primaryStage.setTitle("My Converter Alura by Antonio Guerrero");
         primaryStage.setScene(scene);
@@ -110,83 +87,119 @@ public class MainFrame extends Application {
     }
     
     private void cConverter(Stage primaryStage) {
-        // Crear una imagen
-        Label label = new Label("cagada");
+        
+        TextField number1 = new TextField();
+        number1.getStyleClass().add("number");
 
-        // Crear la sección de la imagen centrada
-        StackPane imageSection = new StackPane(label);
-        imageSection.setAlignment(Pos.CENTER);
+        // Crear un TextFormatter que solo permita números
+        TextFormatter<Integer> textFormatter = new TextFormatter<>(
+            new IntegerStringConverter(),
+            null,
+            change -> {
+                String newText = change.getControlNewText();
+                if (newText.matches("[0-9]*|\\d+\\.\\d*")) { // Expresión regular para números
+                    return change;
+                }
+                return null;
+            }
+        );
+        
+        number1.setTextFormatter(textFormatter);
+
+        ComboBox<String> option1 = new ComboBox<>();
+        option1.setPromptText("Moneda");
+        option1.getItems().addAll(Currency.currencies);
+        option1.getStyleClass().add("option");
+        
+        HBox input = new HBox(5, number1,option1);
+        
+        TextField number2 = new TextField();
+        number2.getStyleClass().add("number");
+        number2.setEditable(false);
+        
+        ComboBox<String> option2 = new ComboBox<>();
+        option2.setPromptText("Moneda");
+        option2.getItems().addAll(Currency.currencies);
+        option2.getStyleClass().add("option");
+        
+        HBox output = new HBox(0, number2,option2);
+
+        Text text = new Text("- Convertir a -");
+        text.setFill(Color.WHITE);
+        text.setFont(Font.font(20));
+
+        // Crear la sección de las funciones
+        HBox optionSection = new HBox(40, input, text,output);
+        optionSection.setAlignment(Pos.CENTER);
 
         // Crear los botones
-        Button button1 = new Button("Divisas");
+        Button button1 = new Button("Menú");
         button1.setOnAction(e -> start(primaryStage));
-        
-        // Asignar la clase de estilo a los botones
         button1.getStyleClass().add("button-style");
+        configureButtonAnimation(button1);
         
-        // Establecer el estilo normal del botón
-        button1.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        
-        // Agregar un efecto hover utilizando eventos de mouse
-        button1.setOnMouseEntered(event -> {
-            button1.setStyle("-fx-background-color: #050505; -fx-text-fill: white;");
-        });
-        
-
-        button1.setOnMouseExited(event -> {
-            button1.setStyle("-fx-background-color: #c74634; -fx-text-fill: black;");
-        });
-        
-        // Cambia el tamaño de fuente del botón
-        button1.setFont(new Font(20));
+        Button button2 = new Button("Convertir");
+        /* button2.setOnAction(e -> ); */
+        button2.getStyleClass().add("button-style");
+        configureButtonAnimation(button2);
         
         // Crear la sección de los botones y el pie de página
-        HBox buttonsSection = new HBox(80, button1);
+        HBox buttonsSection = new HBox(70, button1, button2);
         buttonsSection.setAlignment(Pos.CENTER);
 
         //Crear el contenedor del los elementos principales
-        VBox contentSection = new VBox(0, imageSection, buttonsSection);
+        VBox contentSection = new VBox(30, optionSection, buttonsSection);
         contentSection.setAlignment(Pos.TOP_CENTER);
         
         
         //Crear la sección del footer
         Text footerText = new Text("Por Antonio Guerrero para AluraLatam y Oracle, 2023");
-        footerText.setFont(Font.font(18));
+        footerText.getStyleClass().add("footer-text");
+
         HBox footer = new HBox(footerText);
+        footer.getStyleClass().add("footer");
         footer.setAlignment(Pos.BOTTOM_CENTER);
-        footer.setPadding(new Insets(20, 0, 20, 0));
-        
-        //color del footer
-        BackgroundFill backgroundF = new BackgroundFill(Color.rgb(199,70,52), null, null);
-        Background background2 = new Background(backgroundF);
-        footer.setBackground(background2);
-        
         
         // Crear el contenedor principal
-        VBox root = new VBox(80, contentSection, footer);
+        VBox root = new VBox(100, contentSection, footer);
+        root.getStyleClass().add("root");
         root.setAlignment(Pos.BOTTOM_CENTER);
         
-        //Color del Root
-        BackgroundFill backgroundR = new BackgroundFill(Color.rgb(22,21,19), null, null);
-        Background background1 = new Background(backgroundR);
-        root.setBackground(background1);
 
         // Crear la escena y configurarla en el escenario
         Scene sceneCurrency = new Scene(root, 960, 540);
+        sceneCurrency.getStylesheets().add("file:///A:/programacion/java/MyConversorbyAntonioGG/MyConverterAlura/src/main/resources/style.css");
 
         primaryStage.setScene(sceneCurrency);
     }
 
-    private void dConverter() {
-        
-    }
+private void dConverter() {
+    
+}
 
-    private void tConverter() {
-        
-    }
+private void tConverter() {
+    
+}
 
+public void configureButtonAnimation(Button button) {
+    // Crear una transición de escala al hacer hover
+    ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(400), button);
+    scaleTransition.setToX(1.5);
+    scaleTransition.setToY(1.5);
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    // Configurar el evento de hover para activar la animación
+    button.setOnMouseEntered(event -> scaleTransition.play());
+
+    // Configurar el evento de salida del mouse para revertir la animación
+    button.setOnMouseExited(event -> {
+        ScaleTransition reverseTransition = new ScaleTransition(Duration.millis(400), button);
+        reverseTransition.setToX(1.0);
+        reverseTransition.setToY(1.0);
+        reverseTransition.play();
+    });
+}
+
+public static void main(String[] args) {
+    launch(args);
+}
 }
